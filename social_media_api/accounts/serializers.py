a1bbc8b04823a2a7c83ac56726fc4be9289f5e4c
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-21 09:15:26 +0300

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -7,6 +7,16 @@ class UserRegistrationSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField()
 
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        # Create a token for the user
+        Token.objects.create(user=user)
+        return user
+
 class CustomAuthTokenSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
