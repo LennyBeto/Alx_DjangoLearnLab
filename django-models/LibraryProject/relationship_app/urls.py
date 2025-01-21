c31c0f9b8f9bdb731ad929c1a05eba2861522e26
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-21 06:50:45 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from .admin_view import admin_view
+from .librarian_view import librarian_view
+from .member_view import member_view
 from .views import list_books
-
+from .templates import register.html
+from .templates import login.html
+from .templates import logout.html
 
 
 urlpatterns = [
@@ -26,4 +31,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='templates/logout.html'), name='logout'),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
 ]
