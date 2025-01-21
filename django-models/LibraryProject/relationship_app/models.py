3f01c255882aca46a7920c70d9e5f64f4673dddd
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-21 06:42:55 +0300

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,16 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class Author(models.Model):
+   class UserProfile(models.Model):
+       ROLE_CHOICES = [
+           ('Admin', 'Admin'),
+           ('Librarian', 'Librarian'),
+           ('Member', 'Member'),
+       ]
+       user = models.OneToOneField(User, on_delete=models.CASCADE)
+       role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+   @receiver(post_save, sender=User)
+   def create_user_profile(sender, instance, created, **kwargs):
+       if created:
+           UserProfile.objects.create(user=instance)
+
+   @receiver(post_save, sender=User)
+   def save_user_profile(sender, instance, **kwargs):
+       instance.userprofile.save()
+          
+   class Author(models.Model):
        return self.name = models.CharField(max_length=100)
 
-class Book(models.Model):
+   class Book(models.Model):
        title = models.CharField(max_length=200)
        author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
-class Library(models.Model):
+   class Library(models.Model):
        name = models.CharField(max_length=100)
        books = models.ManyToManyField(Book)
 
-class Librarian(models.Model):
+   class Librarian(models.Model):
        name = models.CharField(max_length=100)
        library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+
