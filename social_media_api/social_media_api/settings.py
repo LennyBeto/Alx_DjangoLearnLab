f4325136356f9a70af2882cfac0cc77b0a9ad8e5
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-21 08:59:15 +0300

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.rest_framework',
+    'django.contrib.accounts',
+    'django.contrib.rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
