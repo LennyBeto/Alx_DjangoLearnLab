811037db74dfb6e328c2709193b581efe02eecdc
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-21 06:45:04 +0300

    Create admin_view.py

---
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -0,0 +1,6 @@
+ from django.contrib.auth.decorators import user_passes_test
+   from django.shortcuts import render
+
+   @user_passes_test(lambda u: u.userprofile.role == 'Admin')
+   def admin_view(request):
+       return render(request, 'admin_dashboard.html')
