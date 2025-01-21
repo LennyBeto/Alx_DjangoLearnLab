2681dadf60cd5b9531ff62e208084f7a3f781be6
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-21 09:05:22 +0300

    Update serializers.py

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = ('key',)
