fedbf8a5c31a324bf81a3914905b1daab2e46e37
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-21 06:53:32 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path
 from .admin_view import admin_view
 from .librarian_view import librarian_view
 from .member_view import member_view
