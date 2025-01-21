1aaaf6f5d2648c3a6245d2c9d57c6ab99e00f90f
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-21 07:03:39 +0300

    Update admin_view.py

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,6 +1,11 @@
- from django.contrib.auth.decorators import user_passes_test
-   from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
 
-   @user_passes_test(lambda u: u.userprofile.role == 'Admin')
-   def admin_view(request):
-       return render(request, 'admin_dashboard.html')
+# Custom test function to check if the user is an admin
+def is_admin(user):
+    return user.is_authenticated and user.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    # Your admin view logic here
+    return render(request, 'admin_dashboard.html')
