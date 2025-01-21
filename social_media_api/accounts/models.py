299247fc32cac9b57485d3949bceef77db117ba9
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-21 09:04:07 +0300

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,8 @@
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False, blank=True)
