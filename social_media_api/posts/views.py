6444728d71ef4b04a6340ef6f7057080aeb837b0
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-22 14:46:26 +0300

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import filters
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+class PostViewSet(viewsets.ModelViewSet):
+    ...
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ['title', 'content']
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        return Post.objects.filter(author__in=self.request.user.following.all())
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
