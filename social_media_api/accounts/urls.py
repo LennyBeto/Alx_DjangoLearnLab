855ae77537b66629d76a216947b37e8db0cbf8c9
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-22 16:47:52 +0300

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import user_registration, CustomAuthToken, user_profile
+from .views import user_registration, CustomAuthToken, user_profile, FollowView, UnfollowView
 
 urlpatterns = [
     path('register/', user_registration, name='user-registration'),
     path('login/', CustomAuthToken.as_view(), name='user-login'),
     path('profile/', user_profile, name='user-profile'),
+    path('follow/<int:user_id>/', FollowView.as_view(), name='follow'),
+    path('unfollow/<int:user_id>/', UnfollowView.as_view(), name='unfollow'),
 ]
+
