b33827bfbc5a993ece4d881ee2198c0ebdd0fd2b
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-22 15:22:46 +0300

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,8 +1,16 @@
-from rest_framework import status
+from rest_framework import status, permissions, generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Post, Like
 from notifications.models import Notification
+
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return generics.get_object_or_404(Post, pk=self.kwargs['pk'])
 
 @api_view(['POST'])
 def like_post(request, pk):
