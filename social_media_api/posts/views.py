09518e36aa7e623ae280028d224dd0923643d518
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-22 15:25:41 +0300

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -12,23 +12,4 @@ class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     def get_object(self):
         return generics.get_object_or_404(Post, pk=self.kwargs['pk'])
 
-@api_view(['POST'])
-def like_post(request, pk):
-    post = Post.objects.get(pk=pk)
-    like, created = Like.objects.get_or_create(user=request.user, post=post)
 
-    if created:
-        Notification.objects.create(recipient=post.user, actor=request.user, verb='liked', target=post)
-        return Response({'status': 'liked'}, status=status.HTTP_201_CREATED)
-    else:
-        return Response({'status': 'already liked'}, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['DELETE'])
-def unlike_post(request, pk):
-    post = Post.objects.get(pk=pk)
-    try:
-        like = Like.objects.get(user=request.user, post=post)
-        like.delete()
-        return Response({'status': 'unliked'}, status=status.HTTP_204_NO_CONTENT)
-    except Like.DoesNotExist:
-        return Response({'status': 'not liked'}, status=status.HTTP_400_BAD_REQUEST)
