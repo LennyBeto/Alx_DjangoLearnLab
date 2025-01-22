5e89768b9182c2b0b8c8197073421c7384d7f6d9
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-22 14:52:45 +0300

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class CustomUser(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, blank=True)
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers' blank=True)
