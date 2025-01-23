1dbe2aceeedfe66954eee49c5133bcc2d6ef2925
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-23 12:02:21 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Book 
 
+from .forms import ExampleForm  
+
 def book_list(request):
+
     try:
         books = Book.objects.all()  # Fetch all books from the database
         return render(request, 'bookshelf/book_list.html', {'books': books})
     except Exception as e:
         raise_exception(e)  
+    pass
