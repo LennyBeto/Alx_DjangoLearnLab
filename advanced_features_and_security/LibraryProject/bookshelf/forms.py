1fad6bbf1119ea99a9e015c66d95190ccccb0975
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-23 11:59:57 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,1 +1,10 @@
+from django import forms
+from .models import Book  
 
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book  
+        fields = ['title', 'author', 'description']  
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4, 'cols': 15}),
+        }
