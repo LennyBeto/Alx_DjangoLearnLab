2ecac7a1d7f4ae9678a1f600425716b425b9f85b
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-23 18:21:15 +0300

    Update models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+class Post(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
