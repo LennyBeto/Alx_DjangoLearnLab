d6f83bf64f170026531cdc2c316bd3f970ca77cc
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-23 18:42:56 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,7 +5,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters import rest_framework
+from django_filters import rest_framework as filters
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
