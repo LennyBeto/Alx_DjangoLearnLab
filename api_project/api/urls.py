818e81d2556dfb0d83dfaf67f2f87d12a09951d8
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-23 12:50:40 +0300

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,1 +1,35 @@
+"""
+URL configuration for api_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet, BookList
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Includes the api app's URLs
+    path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),  # This includes all routes registered with the router
+    path('api-token-auth/', obtain_auth_token),
+]
 
