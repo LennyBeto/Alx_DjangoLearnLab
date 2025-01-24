e1b8d9941d499b7e3e5b927f2d9578e490b318a7
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-24 03:35:37 +0300

    Update test_views.py

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -12,20 +12,20 @@ class BookAPITestCase(APITestCase):
         self.book_data = {'title': 'Test Book', 'author': 'Author Name', 'published_date': '2025-01-01'}
 
     def test_create_book(self):
-        response = self.client.post(reverse('book-list'), self.book_data)
+        response.data = self.client.post(reverse('book-list'), self.book_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Book.objects.count(), 1)
         self.assertEqual(Book.objects.get().title, 'Test Book')
 
     def test_update_book(self):
         book = Book.objects.create(**self.book_data)
-        response = self.client.put(reverse('book-detail', args=[book.id]), {'title': 'Updated Book'})
+        response.data = self.client.put(reverse('book-detail', args=[book.id]), {'title': 'Updated Book'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         book.refresh_from_db()
         self.assertEqual(book.title, 'Updated Book')
 
     def test_delete_book(self):
         book = Book.objects.create(**self.book_data)
-        response = self.client.delete(reverse('book-detail', args=[book.id]))
+        response.data = self.client.delete(reverse('book-detail', args=[book.id]))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Book.objects.count(), 0)
