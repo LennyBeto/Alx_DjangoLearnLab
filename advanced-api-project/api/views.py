f0610cc13c3edaffbdf309230865a477e11d28c0
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-29 23:24:04 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,12 +5,10 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters import rest_framework as filters
 
 
 from django_filters import rest_framework as filters
 from rest_framework import generics, permissions, filters as drf_filters
-from .models import Book
 from .serializers import BookSerializer
 
 class BookFilter(filters.FilterSet):
